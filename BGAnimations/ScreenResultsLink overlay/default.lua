8531ad91db1f9f96d189efdafe21ae955a7589e7
domicat-0 <domicat7@gmail.com>
2024-08-16 14:56:05 -0500

    Fix results screen

--- BGAnimations/ScreenResultsLink overlay/default.lua
+++ BGAnimations/ScreenResultsLink overlay/default.lua
@@ -1,16 +1,20 @@
 local t = Def.ActorFrame {
 	InitCommand=function(self)
 		self:xy(_screen.cx, _screen.cy)
-	end,
+	end
 }
 
 t[#t+1] = Def.Quad {
-	InitCommand=function(self) self:FullScreen():diffuse(0,0,0,0.85):xy(0, 0) end
+	InitCommand=function(self) 
+		self:FullScreen():diffuse(0,0,0,0.85):xy(0, 0) 
+	end
 }
 
-for tag in ivalues(SL.Global.LinkPlayerList) do
-	local score = data[tag]["score"]
-	LoadActor("./resultbar.lua", {tag, score})
+local idx = 1
+for tag, score in pairs(SL.Global.LinkPlayerScores) do
+	t[#t+1] = LoadActor("./resultbar.lua", {tag, score, -150, -100 + idx*50})
+	SCREENMAN:SystemMessage(SL.Global.LinkPlayerTag)
+	idx = idx + 1
 end
 
 return t
--- BGAnimations/ScreenResultsLink overlay/resultbar.lua
+++ BGAnimations/ScreenResultsLink overlay/resultbar.lua
@@ -1,28 +1,36 @@
 local args = ...
 local tag = args[1]
 local score = args[2]
+local pos_x = args[3]
+local pos_y = args[4]
 
-local width = 70
-local height = 30
+local width = 240
+local height = 40
 local player_color = color("FF4D4D")
 local opponent_color = color("4D4DFF")
 
 local af = Def.ActorFrame {
-	
+	InitCommand = function(self)
+		self:x(pos_x):y(pos_y)
+	end
 }
 
 
 af[#af+1] = Def.Quad {
 	InitCommand = function(self)
-		self:zoomto(width+6, height+2)
-		self:diffuse(0.15, 0.15, 0.15, 1)
+		self:zoomto(width+20, height)
+		if tag == SL.Global.LinkPlayerTag then
+			self:diffuse(0.3, 0.3, 1, 1)
+		else
+			self:diffuse(1, 0.3, 0.3, 1)
+		end
 	end
 }
 
 af[#af+1] = Def.Quad {
 	InitCommand = function(self)
 		self:zoomto(width, height)
-		self:x(6)
+		self:x(10)
 		self:diffuse(0.15, 0.15, 0.15, 1)
 	end
 }
@@ -31,16 +39,16 @@ af[#af+1] = LoadFont("Common Normal")..{
 	Name="",
 	Text=tag,
 	InitCommand = function(self)
-		self:zoom(0.7)
-		self:x(-20)
+		self:zoom(1)
+		self:x(-10)
 	end
 }
 af[#af+1] = LoadFont("Common Normal")..{
 	Name="",
-	Text=tag,
+	Text=score,
 	InitCommand = function(self)
-		self:zoom(0.7)
-		self:x(-20)
+		self:zoom(1.5)
+		self:x(110)
 	end
 }
 
